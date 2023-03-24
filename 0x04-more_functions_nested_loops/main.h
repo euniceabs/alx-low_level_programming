8e3dfe7832e63786b7884d2d3594ecdacd0b81ee
eunicabs <botweunice100@gmail.com>
2023-03-24 16:08:22 -0700

    A function that prints numbers from 0-9

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,7 @@ int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+int _putchar(char c);
 void print_most_numbers(void);
 void more_numbers(void);
 void most_numbers(void);
