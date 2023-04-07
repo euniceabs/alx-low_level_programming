5304697389027d3a636ce908bc83e3490703e77d
eunicabs <botweunice100@gmail.com>
2023-04-06 21:56:04 -0700

    Corrections

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,39 @@
 #include "main.h"
-
-int check_pal(char *s, int i, int len);
-int _strlen_recursion(char *s);
-
 /**
- * is_palindrome - a function that checks if a string is a palindrome
- * @s: string to check
- * Return: 1 if s is a palindrome, 0 if otherwise
- */
-
-int is_palindrome(char *s)
-{
-if (*s == '\0')
-return (1);
-return (is_palindrome(s, 0, _strlen_recursion(s)));
-}
-/**
- * _strlen_recursion - a function that returns the length of a string
- * @s: string to calculate the length of
+ * _strlen_recursion - Prints the length of a string.
+ * @s: the string to be printed
  * Return: length of the string
  */
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-return (0);
-return (1 + _strlen_recursion(s + 1));
+	if (s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
 }
 /**
- * check_palindrome - checks the characters recursively for palindrome
- * @s: string to check
- * @i: number to interate
- * @len: length of the string
- * Return: 1 if s is a palindrome, 0 if otherwise
+ * pal_checker - check for palindrome.
+ * @s: string
+ * @i: left index.
+ * @j: right index.
+ * Return: 1 if s is palindrome or 0 otherwise.
  */
-int check_palindrome(char *s, int i, int len)
+int pal_checker(char *s, int i, int j)
 {
-if (*(s + i) != *(s + len - 1))
-return (0);
-if (i >= len)
-return (1);
-return (check_palindrome(s, i + 1, len - 1));
+	if (s[i] == s[j])
+		if (i > j / 2)
+			return (1);
+		else
+			return (pal_checker(s, i + 1, j - 1));
+	else
+		return (0);
+}
+/**
+ * is_palindrome - check for palindrome
+ * @s: string.
+ *
+ * Return: 1 if n is prime or 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+	return (pal_checker(s, 0, _strlen_recursion(s) - 1));
 }
