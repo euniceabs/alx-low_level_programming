2f1bc5ecfa8aac96fc32ade7098c80270f3b9ff5
eunicabs <botweunice100@gmail.com>
2023-04-20 09:53:15 -0700

    function that returns the sum of all its parameter

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,19 +1,5 @@
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
-
-#include <stdarg.h>
-#include <stdio.h>
-
-/**
- * struct print - print type with corresponding print function
- * @t: print type
- * @f: print function
- */
-typedef struct print
-{
-char *t;
-void (*f)(va_list);
-} print_t;
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -21,4 +7,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#endif 
+#endif
